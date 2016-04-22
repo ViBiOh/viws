03072ba44a5cc0df4d1ba2f13b94bdd9d1e50d90
Vincent Boutour <ViBiOh@users.noreply.github.com>
2016-04-22 13:24:53 +0200

    Refactoring to OwaspMiddleware

--- src/server.go
+++ src/server.go
@@ -10,6 +10,26 @@ const port = "1080"
 const directory = "/www/"
 const tenDaysOfCaching = "864000"
 
+type OwaspHeaderServer struct {
+	http.ResponseWriter
+}
+
+func (w OwaspHeaderServer) WriteHeader(code int) {
+	if code == 200 {
+		w.Header().Add("Content-Security-Policy", "default-src 'self' 'unsafe-inline' http://*.vibioh.fr https://*.vibioh.fr https://apis.google.com https://fonts.googleapis.com https://fonts.gstatic.com")
+		w.Header().Add("X-Frame-Options", "deny")
+		w.Header().Add("X-Content-Type-Options", "nosniff")
+		w.Header().Add("X-XSS-Protection", "1; mode=block")
+	}
+	w.ResponseWriter.WriteHeader(code)
+}
+
+func owaspMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		h.ServeHTTP(OwaspHeaderServer{ResponseWriter: w}, r)
+	})
+}
+
 type CustomFileServer struct {
 	io.Writer
 	http.ResponseWriter
@@ -21,10 +41,6 @@ func (w CustomFileServer) Write(b []byte) (int, error) {
 
 func (w CustomFileServer) WriteHeader(code int) {
 	if code == 200 {
-		w.Header().Add("Content-Security-Policy", "default-src 'self' 'unsafe-inline' http://*.vibioh.fr https://*.vibioh.fr https://apis.google.com https://fonts.googleapis.com https://fonts.gstatic.com")
-		w.Header().Add("X-Frame-Options", "deny")
-		w.Header().Add("X-Content-Type-Options", "nosniff")
-		w.Header().Add("X-XSS-Protection", "1; mode=block")
 		w.Header().Add("Cache-Control", "max-age="+tenDaysOfCaching)
 	}
 	
@@ -55,7 +71,7 @@ func customFileServer(h http.Handler) http.Handler {
 }
 
 func main() {
-	http.Handle("/", customFileServer(http.FileServer(http.Dir(directory))))
+	http.Handle("/", owaspMiddleware(customFileServer(http.FileServer(http.Dir(directory)))))
 
 	log.Println("Starting server on port " + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
