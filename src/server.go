1f7445ad3893273f0c504728d29e3563727de460
Vincent Boutour <ViBiOh@users.noreply.github.com>
2016-04-27 15:45:34 +0200

    Update server.go

--- src/server.go
+++ src/server.go
@@ -11,38 +11,24 @@ const port = "1080"
 const directory = "/www/"
 const tenDaysOfCaching = "864000"
 
-type OwaspHeaderServer struct {
-	http.ResponseWriter
-}
-
-func (w OwaspHeaderServer) WriteHeader(code int) {
-	if code == 200 {
-		w.Header().Add("Content-Security-Policy", "default-src 'self' 'unsafe-inline' http://*.vibioh.fr https://*.vibioh.fr https://apis.google.com https://fonts.googleapis.com https://fonts.gstatic.com")
-		w.Header().Add("X-Frame-Options", "deny")
-		w.Header().Add("X-Content-Type-Options", "nosniff")
-		w.Header().Add("X-XSS-Protection", "1; mode=block")
-	}
-	w.ResponseWriter.WriteHeader(code)
-}
-
-func owaspMiddleware(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		h.ServeHTTP(OwaspHeaderServer{ResponseWriter: w}, r)
-	})
-}
-
-type GzipServer struct {
+type CustomFileServer struct {
 	io.Writer
 	http.ResponseWriter
 }
 
-func (w GzipServer) Write(b []byte) (int, error) {
-	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
+func (w CustomFileServer) Write(b []byte) (int, error) {
 	return w.Writer.Write(b)
 }
 
-func (w GzipServer) WriteHeader(code int) {
+func (w CustomFileServer) WriteHeader(code int) {
 	if code == 200 {
+		w.Header().Add("Cache-Control", "max-age="+tenDaysOfCaching)
+		
+		w.Header().Add("Content-Security-Policy", "default-src 'self' 'unsafe-inline' http://*.vibioh.fr https://*.vibioh.fr https://apis.google.com https://fonts.googleapis.com https://fonts.gstatic.com")
+		w.Header().Add("X-Frame-Options", "deny")
+		w.Header().Add("X-Content-Type-Options", "nosniff")
+		w.Header().Add("X-XSS-Protection", "1; mode=block")
+		
 		w.Header().Add("Vary", "Accept-Encoding")
 		w.Header().Set("Content-Encoding", "gzip")
 	}
@@ -50,7 +36,7 @@ func (w GzipServer) WriteHeader(code int) {
 	w.ResponseWriter.WriteHeader(code)
 }
 
-func gzipMiddleware(h http.Handler) http.Handler {
+func customMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 			h.ServeHTTP(w, r)
@@ -60,25 +46,7 @@ func gzipMiddleware(h http.Handler) http.Handler {
 		gz := gzip.NewWriter(w)
 		defer gz.Close()
 
-		h.ServeHTTP(GzipServer{ResponseWriter: w, Writer: gz}, r)
-	})
-}
-
-type CustomFileServer struct {
-	http.ResponseWriter
-}
-
-func (w CustomFileServer) WriteHeader(code int) {
-	if code == 200 {
-		w.Header().Add("Cache-Control", "max-age="+tenDaysOfCaching)
-	}
-
-	w.ResponseWriter.WriteHeader(code)
-}
-
-func customMiddleware(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		h.ServeHTTP(CustomFileServer{ResponseWriter: w}, r)
+		h.ServeHTTP(CustomFileServer{ResponseWriter: w, Writer: gz}, r)
 	})
 }
 
