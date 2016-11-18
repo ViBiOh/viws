c83ba88a7ace52fd59ccb80b6875912fb6927904
Vincent Boutour <bob@vibioh.fr>
2016-11-18 22:23:00 +0100

    Changing file server

--- src/server.go
+++ src/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -11,25 +10,26 @@ import (
 
 const tenDaysOfCaching = `864000`
 const contentSecurityPolicy = `default-src 'self' 'unsafe-inline' `
-const pathToServe = `/`
-const indexFileName = `index.html`
 
-var directory string
-var port string
 var domain string
-var static string
-var notFoundName string
+var spa bool
+var notFound bool
 var notFoundPath string
 
-var customNotFound bool
-var spa bool
+func isFileExist(directory string, pathToTest string) (string, bool) {
+	fullPath := path.Join(directory, pathToTest)
+	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+		return fullPath, false
+	}
+	return fullPath, true
+}
 
 type OwaspMiddleware struct {
 	http.ResponseWriter
 }
 
 func (m *OwaspMiddleware) WriteHeader(status int) {
-	if status < http.StatusBadRequest || customNotFound {
+	if status < http.StatusBadRequest {
 		m.Header().Add(`Content-Security-Policy`, contentSecurityPolicy+domain)
 		m.Header().Add(`X-Frame-Options`, `deny`)
 		m.Header().Add(`X-Content-Type-Options`, `nosniff`)
@@ -51,101 +51,46 @@ func (handler OwaspHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	handler.h.ServeHTTP(&OwaspMiddleware{ResponseWriter: w}, r)
 }
 
-type CustomMiddleware struct {
-	http.ResponseWriter
-	isNotFound bool
+type CustomFileHandler struct {
+	root string
 }
 
-func (m *CustomMiddleware) WriteHeader(status int) {
-	if status == http.StatusNotFound && customNotFound {
-		m.isNotFound = true
-		m.Header().Add(`Content-type`, `text/html; charset=utf-8`)
-	}
-
-	m.ResponseWriter.WriteHeader(status)
-}
-
-func (m *CustomMiddleware) Write(p []byte) (int, error) {
-	if m.isNotFound {
-		if notFoundPage, err := ioutil.ReadFile(notFoundPath); err == nil {
-			return m.ResponseWriter.Write(notFoundPage)
-		}
-	}
-	return m.ResponseWriter.Write(p)
-}
-
-type CustomHandler struct {
-	h http.Handler
-}
-
-func (handler CustomHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	handler.h.ServeHTTP(&CustomMiddleware{ResponseWriter: w}, r)
-}
-
-type SpaMiddleware struct {
-	http.ResponseWriter
-	isNotFound bool
-}
-
-func (m *SpaMiddleware) WriteHeader(status int) {
-	if status == http.StatusNotFound {
-		m.isNotFound = true
-		m.Header().Add(`Content-type`, `text/html; charset=utf-8`)
-	}
-
-	m.ResponseWriter.WriteHeader(status)
-}
-
-func (m *SpaMiddleware) Write(p []byte) (int, error) {
-	if m.isNotFound {
-		if indexPage, err := ioutil.ReadFile(directory + indexFileName); err == nil {
-			return m.ResponseWriter.Write(indexPage)
-		}
-	}
-	return m.ResponseWriter.Write(p)
-}
-
-type SpaHandler struct {
-	h http.Handler
-}
-
-func (handler SpaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	handler.h.ServeHTTP(&SpaMiddleware{ResponseWriter: w}, r)
-}
-
-func checkCustomNotFound() {
-	if _, err := os.Stat(notFoundPath); os.IsNotExist(err) {
-		log.Println(notFoundPath + ` is not found. Flag ignored.`)
-		customNotFound = false
+func (handler CustomFileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if filePath, valid := isFileExist(handler.root, r.URL.Path); valid {
+		http.ServeFile(w, r, filePath)
+	} else if notFound {
+		http.ServeFile(w, r, notFoundPath)
+	} else if spa {
+		http.ServeFile(w, r, handler.root)
 	} else {
-		log.Println(`404 page is ` + notFoundPath)
+		http.Error(w, `404 not found: `+r.URL.Path, 500)
 	}
 }
 
 func main() {
+	port := flag.String(`port`, `1080`, `Listening port`)
+	directory := flag.String(`directory`, `/www/`, `Directory to serve`)
 	flag.BoolVar(&spa, `spa`, false, `Indicate Single Page Application mode`)
-	flag.BoolVar(&customNotFound, `notFound`, false, `Graceful 404 page at /404.html`)
+	flag.BoolVar(&notFound, `notFound`, false, `Graceful 404 page at /404.html`)
 	flag.StringVar(&domain, `domain`, ``, `Domains names for Content-Security-Policy`)
-	flag.StringVar(&directory, `directory`, `/www/`, `Directory to serve`)
-	flag.StringVar(&notFoundName, `notFoundName`, `404.html`, `Page served when notFound enabled`)
-	flag.StringVar(&port, `port`, `1080`, `Listening port`)
 	flag.Parse()
 
-	log.Println(`Starting server on port ` + port)
+	log.Println(`Starting server on port ` + *port)
+	log.Println(`Serving file from ` + *directory)
 	log.Println(`Content-Security-Policy: `, contentSecurityPolicy+domain)
-	log.Println(`Serving file from ` + path.Join(directory, pathToServe))
 
 	if spa {
 		log.Println(`Working in SPA mode`)
-		http.Handle(pathToServe, SpaHandler{OwaspHandler{(http.FileServer(http.Dir(directory)))}})
-	} else {
-		http.Handle(pathToServe, CustomHandler{OwaspHandler{(http.FileServer(http.Dir(directory)))}})
 	}
 
-	if customNotFound {
-		notFoundPath = path.Join(directory, pathToServe, notFoundName)
-		checkCustomNotFound()
+	if notFound {
+		if notFoundPath, valid := isFileExist(*directory, `404.html`); !valid {
+			log.Println(notFoundPath + ` is not found. Flag ignored.`)
+			notFound = false
+		}
 	}
 
-	log.Fatal(http.ListenAndServe(`:`+port, nil))
+	http.Handle(`/`, OwaspHandler{CustomFileHandler{*directory}})
+
+	log.Fatal(http.ListenAndServe(`:`+*port, nil))
 }
