1c219c579c053a4ed8661251444bd44d3410df0f
Vincent <ViBiOh@users.noreply.github.com>
2016-12-14 11:29:11 +0100

    Update server.go

--- server.go
+++ server.go
@@ -1,118 +1,118 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"net/http"
-	"os"
-	"path"
-	"runtime"
+  "flag"
+  "log"
+  "net/http"
+  "os"
+  "path"
+  "runtime"
 )
 
 const tenDaysOfCaching = `864000`
 const contentSecurityPolicy = `default-src 'self' 'unsafe-inline' `
-const NOT_FOUND_FILENAME = `404.html`
-const INDEX_FILENAME = `index.html`
+const notFoundFilename = `404.html`
+const indexFilename = `index.html`
 
 var domain string
 
 func isFileExist(parts ...string) *string {
-	fullPath := path.Join(parts...)
-	info, err := os.Stat(fullPath)
+  fullPath := path.Join(parts...)
+  info, err := os.Stat(fullPath)
 
-	if err != nil {
-		return nil
-	}
+  if err != nil {
+    return nil
+  }
 
-	if info.IsDir() {
-		if isFileExist(append(parts, INDEX_FILENAME)...) == nil {
-			return nil
-		}
-	}
+  if info.IsDir() {
+    if isFileExist(append(parts, indexFilename)...) == nil {
+      return nil
+    }
+  }
 
-	return &fullPath
+  return &fullPath
 }
 
-type OwaspMiddleware struct {
-	http.ResponseWriter
+type owaspMiddleware struct {
+  http.ResponseWriter
 }
 
-func (m *OwaspMiddleware) WriteHeader(status int) {
-	if status < http.StatusBadRequest {
-		m.Header().Add(`Content-Security-Policy`, contentSecurityPolicy+domain)
-		m.Header().Add(`X-Frame-Options`, `deny`)
-		m.Header().Add(`X-Content-Type-Options`, `nosniff`)
-		m.Header().Add(`X-XSS-Protection`, `1; mode=block`)
-	}
+func (m *owaspMiddleware) WriteHeader(status int) {
+  if status < http.StatusBadRequest {
+    m.Header().Add(`Content-Security-Policy`, contentSecurityPolicy+domain)
+    m.Header().Add(`X-Frame-Options`, `deny`)
+    m.Header().Add(`X-Content-Type-Options`, `nosniff`)
+    m.Header().Add(`X-XSS-Protection`, `1; mode=block`)
+  }
 
-	if status == http.StatusOK || status == http.StatusMovedPermanently {
-		m.Header().Add(`Cache-Control`, `max-age=`+tenDaysOfCaching)
-	}
+  if status == http.StatusOK || status == http.StatusMovedPermanently {
+    m.Header().Add(`Cache-Control`, `max-age=`+tenDaysOfCaching)
+  }
 
-	m.ResponseWriter.WriteHeader(status)
+  m.ResponseWriter.WriteHeader(status)
 }
 
 type OwaspHandler struct {
-	h http.Handler
+  h http.Handler
 }
 
 func (handler OwaspHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	handler.h.ServeHTTP(&OwaspMiddleware{ResponseWriter: w}, r)
+  handler.h.ServeHTTP(&owaspMiddleware{ResponseWriter: w}, r)
 }
 
-type CustomFileHandler struct {
-	root         *string
-	spa          bool
-	notFound     bool
-	notFoundPath *string
+type customFileHandler struct {
+  root         *string
+  spa          bool
+  notFound     bool
+  notFoundPath *string
 }
 
-func (handler CustomFileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if filePath := isFileExist(*handler.root, r.URL.Path); filePath != nil {
-		http.ServeFile(w, r, *filePath)
-	} else if handler.notFound {
-		http.ServeFile(w, r, *handler.notFoundPath)
-	} else if handler.spa {
-		http.ServeFile(w, r, *handler.root)
-	} else {
-		http.Error(w, `404 page not found: `+r.URL.Path, 404)
-	}
+func (handler customFileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  if filePath := isFileExist(*handler.root, r.URL.Path); filePath != nil {
+    http.ServeFile(w, r, *filePath)
+  } else if handler.notFound {
+    http.ServeFile(w, r, *handler.notFoundPath)
+  } else if handler.spa {
+    http.ServeFile(w, r, *handler.root)
+  } else {
+    http.Error(w, `404 page not found: `+r.URL.Path, 404)
+  }
 }
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
+  runtime.GOMAXPROCS(runtime.NumCPU())
 
-	port := flag.String(`port`, `1080`, `Listening port`)
-	directory := flag.String(`directory`, `/www/`, `Directory to serve`)
-	spa := flag.Bool(`spa`, false, `Indicate Single Page Application mode`)
-	notFound := flag.Bool(`notFound`, false, `Graceful 404 page at /404.html`)
-	flag.StringVar(&domain, `domain`, ``, `Domains names for Content-Security-Policy`)
-	flag.Parse()
+  port := flag.String(`port`, `1080`, `Listening port`)
+  directory := flag.String(`directory`, `/www/`, `Directory to serve`)
+  spa := flag.Bool(`spa`, false, `Indicate Single Page Application mode`)
+  notFound := flag.Bool(`notFound`, false, `Graceful 404 page at /404.html`)
+  flag.StringVar(&domain, `domain`, ``, `Domains names for Content-Security-Policy`)
+  flag.Parse()
 
-	if isFileExist(*directory) == nil {
-		log.Fatal(`Directory ` + *directory + ` is unreachable.`)
-	}
+  if isFileExist(*directory) == nil {
+    log.Fatal(`Directory ` + *directory + ` is unreachable.`)
+  }
 
-	log.Println(`Starting server on port ` + *port)
-	log.Println(`Serving file from ` + *directory)
-	log.Println(`Content-Security-Policy: `, contentSecurityPolicy+domain)
+  log.Println(`Starting server on port ` + *port)
+  log.Println(`Serving file from ` + *directory)
+  log.Println(`Content-Security-Policy: `, contentSecurityPolicy+domain)
 
-	if *spa {
-		log.Println(`Working in SPA mode`)
-	}
+  if *spa {
+    log.Println(`Working in SPA mode`)
+  }
 
-	var notFoundPath *string
+  var notFoundPath *string
 
-	if *notFound {
-		if notFoundPath = isFileExist(*directory, NOT_FOUND_FILENAME); notFoundPath == nil {
-			log.Println(*directory + NOT_FOUND_FILENAME + ` is unreachable. Flag ignored.`)
-			*notFound = false
-		} else {
-			log.Println(`404 will be ` + *notFoundPath)
-		}
-	}
+  if *notFound {
+    if notFoundPath = isFileExist(*directory, notFoundFilename); notFoundPath == nil {
+      log.Println(*directory + notFoundFilename + ` is unreachable. Flag ignored.`)
+      *notFound = false
+    } else {
+      log.Println(`404 will be ` + *notFoundPath)
+    }
+  }
 
-	http.Handle(`/`, OwaspHandler{CustomFileHandler{directory, *spa, *notFound, notFoundPath}})
+  http.Handle(`/`, OwaspHandler{customFileHandler{directory, *spa, *notFound, notFoundPath}})
 
-	log.Fatal(http.ListenAndServe(`:`+*port, nil))
+  log.Fatal(http.ListenAndServe(`:`+*port, nil))
 }
